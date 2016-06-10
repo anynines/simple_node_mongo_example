48932b8d70aad00bd00137d60f2939b8b111b132
Patrick Hartz <phartz@avarteq.de>
2016-06-10 12:29:25 +0200

    Solved an issue when user credentials was read by helper.js. New env variable added to locate the main mongodb node in VCAP_SERVICES. The first node will be taken to configure the credentials for the db access. Its a bit pretty!

--- bin/helper.js
+++ bin/helper.js
@@ -1,15 +1,16 @@
-var Quote = require('../db/quote.js'); 
+var Quote = require('../db/quote.js');
 var seeds = require('../db/seeds.js');
 
 get_mongo_url = function () {
   try {
 		var mongodb_service_name = process.env.MONGODB_SERVICE_NAME;
+    console.log(process.env.MONGODB_SERVICE_NAME)
     var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-    mongo_url = vcap_services[mongodb_service_name][0].credentials.uri;
+    mongo_url = vcap_services[process.env.MONGODB_SERVICE][0].credentials.uri;
     return mongo_url;
 	} catch (err) {
 		console.log("An error occured while loading the MongoDB credentials from the env:", err)
-		throw Error(err); 
+		throw Error(err);
 	}
 }
 
@@ -18,9 +19,9 @@ seed_db = function (params) {
 		if(count === 0) {
 			console.log("Inserting seeds into the database!");
 			for(var i = 0; i < seeds.length; i++) {
-				var quote = new Quote({ 
-          author: seeds[i].author, 
-          quote: seeds[i].quote 
+				var quote = new Quote({
+          author: seeds[i].author,
+          quote: seeds[i].quote
         });
 				quote.save(function (err, quote) {
 					if (err) return console.log(err);
